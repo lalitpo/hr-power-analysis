c1834351bfc5121803e42ba626d394ef38403907
lalitpoddar <lalit.dar4@gmail.com>
2023-05-02 10:01:01 +0200

    Storing records in mongo

--- src/repositories/PowerAndHRRepository.py
+++ src/repositories/PowerAndHRRepository.py
@@ -6,14 +6,16 @@ def store_athlete_info(record, db):
     pass
 
 
-def store_athlete_activity(record, db):
+def store_athlete_activity(record, athletic_data_coll):
+    athletic_data_coll.insert_one(record)
     pass
 
 
 def store_records(record):
     hr_power_db = connect_database(configs.get("hr-power-db-url").data)
     store_athlete_info(record, hr_power_db)
-    store_athlete_activity(record, hr_power_db)
+    athletic_data = hr_power_db["Athletic-Data"]
+    store_athlete_activity(record, athletic_data)
 
 
 def connect_database(db_uri):
@@ -23,6 +25,6 @@ def connect_database(db_uri):
     try:
         client.admin.command('ping')
         print("You are successfully connected to your Database!")
-        return client["hr-power-db"]
+        return client["hrr-power-db"]
     except Exception as e:
         print(e)
--- src/services/FetchDataService.py
+++ src/services/FetchDataService.py
@@ -1,6 +1,7 @@
 from src.services.LoginStravaService import *
 from src.repositories.PowerAndHRRepository import *
 import json
+import time
 
 stream_types = configs.get("stream_types").data.split(",")
 
@@ -9,8 +10,8 @@ def get_activity_url(activity_id):
     stream_types_paramlist = ""
     for param in stream_types:
         stream_types_paramlist = stream_types_paramlist + "stream_types%5B%5D=" + param + "&"
-    stream_types_paramlist = stream_types_paramlist.removesuffix("$")
-    return configs.get("strava-url").data + "/activities/" + activity_id + "/streams?" + stream_types_paramlist.__str__()
+    stream_types_paramlist = stream_types_paramlist.removesuffix("&")
+    return configs.get("strava-url").data + "/activities/" + activity_id + "/streams?" + stream_types_paramlist
 
 
 activity_ids = open('../../resources/activity-id.txt').readlines()
@@ -20,10 +21,13 @@ data = ""
 for activity in activity_ids:
     url = get_activity_url(activity.strip())
     print(url)
-    driver.implicitly_wait(10)  # latency so that strava doesn't block us for scraping using a bot.
+    time.sleep(10)  # latency so that strava doesn't block us for scraping using a bot.
     driver.get(url)
     pre = driver.find_element(By.TAG_NAME, "pre").text
     data = json.loads(pre)
+    activity_info = {"activity_id": activity.strip()}
+    data.update(activity_info)
     print(data)
+    store_records(data)
 
-store_records(data)
+
