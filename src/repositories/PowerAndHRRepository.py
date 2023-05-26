95f4175c3a23a13539a4cd365c59ab8d43cb2c61
lalitpoddar <lalit.dar4@gmail.com>
2023-05-26 11:37:53 +0200

    Data Fetching 7

--- src/repositories/PowerAndHRRepository.py
+++ src/repositories/PowerAndHRRepository.py
@@ -1,23 +1,43 @@
-import pymongo
+import psycopg2
+from src.config.LoadProperties import *
 
 
-def store_athlete_info(record, athlete_info_coll):
-    athlete_info_coll.insert_one(record)
-    pass
+def create_table(table1, table2):
+    hr_power_db_conn.cursor().execute(table1)
+    hr_power_db_conn.cursor().execute(table2)
+    hr_power_db_conn.commit()
 
 
-def store_athlete_activity(record, activity_data_coll):
-    activity_data_coll.insert_one(record)
-    pass
-
-
-def connect_database(db_uri):
-    # Create a new client and connect to the server
-    client = pymongo.MongoClient(db_uri)
-    # Send a ping to confirm a successful connection
+def save_data(strava_data, table):
+    keys = strava_data.keys()
+    values = strava_data.values()
+    columns = ', '.join(keys)
+    value_placeholders = ', '.join(['%s'] * len(keys))
+    sql = f'INSERT INTO public."{table}" ({columns}) VALUES ({value_placeholders})'
     try:
-        client.admin.command('ping')
+        hr_power_db_conn.cursor().execute(sql, tuple(values))
+        hr_power_db_conn.commit()
+    except (psycopg2.Error, Exception) as e:
+        # Handle the error
+        print("Error occurred while storing data in PostgresSQL:", str(e))
+
+
+def connect_database(db_name, db_user, db_password):
+    try:
+        # Create a new client and connect to the server
+        conn = psycopg2.connect(host="localhost",
+                                port="5432",
+                                database=db_name,
+                                user=db_user,
+                                password=db_password)
         print("You are successfully connected to your Database!")
-        return client["hrr-power-db"]
+        return conn
     except Exception as e:
         print(e)
+
+
+hr_power_db_conn = connect_database(configs.get("db-name").data, configs.get("db-user").data,
+                                    configs.get("db-pass").data)
+
+create_table(configs.get("athlete-info-table-query").data,
+             configs.get("athletic-data-table-query").data)
--- src/services/FetchDataService.py
+++ src/services/FetchDataService.py
@@ -1,17 +1,16 @@
 import datetime
 import json
+import re
 import time
 import urllib.parse
 from bs4 import BeautifulSoup
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.common import NoSuchElementException
 from src.constants.PowerAndHRConstants import *
 from src.repositories.PowerAndHRRepository import *
 from src.services.LoginStravaService import *
+from _datetime import datetime as dt
 
 athlete_ids = open('../../resources/pro-athlete-id.txt').readlines()
-hr_power_db = connect_database(configs.get("hr-power-db-url").data)
 
 
 def create_activity_url():
@@ -21,6 +20,7 @@ def create_activity_url():
 
 
 base_act_data_url = create_activity_url()
+minimum_activity_length = int(configs.get("min-activity-length-in-sec").data)
 
 
 def create_interval_list():
@@ -42,7 +42,7 @@ def create_interval_list():
 intervals = create_interval_list()
 
 
-def get_weekly_urls(intervals_list):
+def get_weekly_urls():
     base_url = strava_url + "/pros/athlete_id#interval?"
     interval_param = configs.get("activity-interval").data.split(",")
     activity_url_list = []
@@ -55,65 +55,94 @@ def get_weekly_urls(intervals_list):
 
 
 def get_activity_ids(ath_id):
-    weekly_urls = get_weekly_urls(intervals)
+    weekly_urls = get_weekly_urls()
 
     activity_ids_list = []
     for week_url in weekly_urls:
         week_url = week_url.replace(athlete_id, ath_id)
         driver.get(week_url)
-        time.sleep(10)
-        driver.get(week_url)
+        time.sleep(5)
         div_element = driver.find_element(By.CSS_SELECTOR, "div.content.react-feed-component").get_attribute(
             "outerHTML")
         # parse the HTML using BeautifulSoup
         attribute_list = BeautifulSoup(div_element, 'html.parser').contents[0].__getattribute__("attrs")
         activity_list = json.loads(attribute_list["data-react-props"])['appContext']['preFetchedEntries']
 
-        print("Week :" + week_url)
         for act in activity_list:
             if act[entity] == "GroupActivity" and act[rowData][activities][0][activity_type] == ride:
                 activity_ids_list.append(str(act[rowData][activities][0][entity + '_' + id]))
             elif act[entity] == "Activity" and act[activity][activity_type] == ride:
                 activity_ids_list.append(act[activity][id])
-        time.sleep(10)
-        print("Week :" + week_url + " over.")
-    return activity_ids_list
+    return list(set(activity_ids_list))
 
 
-def store_activities_data(athlete_details, activities_list):
+def save_activity_data(activities_list):
     activities_stored = []
     for act in activities_list:
         time.sleep(5)  # latency so that strava doesn't block us for scraping using a bot.
+        driver.get(strava_url + "/activities/" + act)
+        activity_summary = driver.find_element(By.CSS_SELECTOR, "div.details").get_attribute(
+            "outerHTML")
+        meta_data = BeautifulSoup(activity_summary, 'html.parser').text
+        activity_meta_data = re.split(r'\n+', meta_data)
+        main_stats_div = driver.find_element(By.CSS_SELECTOR, "div.spans8.activity-stats.mt-md.mb-md").get_attribute(
+            "outerHTML")
+        main_stats_list = BeautifulSoup(main_stats_div, 'html.parser').contents[0].__getattribute__("contents")
+        head_data = re.split(r'\n+', main_stats_list[1].text)
+        time.sleep(5)  # latency so that strava doesn't block us for scraping using a bot.
         driver.get(base_act_data_url.replace(activity + '_' + id, act))
         pre = driver.find_element(By.TAG_NAME, "pre").text
-        activity_data = json.loads(pre)
-        print("url : " + base_act_data_url.replace(activity + '_' + id, act))
-        if watts in activity_data and len(activity_data[watts]) > 7200:
-            activity_info = {activity + '_' + id: act.strip()}
+        activity_info = json.loads(pre)
+        activity_duration_split = head_data[3].split(':')
+        if len(activity_duration_split) == 2:
+            activity_duration = datetime.timedelta(minutes=int(activity_duration_split[0]),
+                                                   seconds=int(activity_duration_split[1]))
+        else:
+            activity_duration = datetime.timedelta(hours=int(activity_duration_split[0]),
+                                                   minutes=int(activity_duration_split[1]),
+                                                   seconds=int(activity_duration_split[2]))
+        if watts in activity_info:
+            activity_data = {activity + '_' + id: act.strip(),
+                             "activity_date": dt.strptime(activity_meta_data[1].split(', ')[1], '%d %B %Y').date(),
+                             "activity_distance": head_data[1].split(' ')[0],
+                             "activity_duration": str(activity_duration),
+                             "elevation": head_data[5].replace(',', '').split(' ')[0]}
             activity_data.update(activity_info)
-            activity_data_conn = hr_power_db["Activity-Data"]
-            store_athlete_activity(activity_data, activity_data_conn)
+            save_data(activity_data, "athletic-data")
             activities_stored.append(act)
+    return activities_stored
+
+
+def store_activities_data(athlete_details, activities_list):
+    # create the athletic data table with constraints.
+    hr_power_db_conn.cursor().execute(configs.get("athletic-data-table-query").data)
+    # save the athletic data into the athletic-data table created.
+    activities_saved = save_activity_data(activities_list)
     athlete_data = {athlete_name: athlete_details[athlete_name], athlete_id: athlete_details[athlete_id],
-                    location: athlete_details[location], "activities_ids": activities_stored}
-    athlete_info_conn = hr_power_db["Athlete-Info"]
-    store_athlete_info(athlete_data, athlete_info_conn)
+                    location: athlete_details[location], "activities_ids": [int(x) for x in activities_saved]}
+    save_data(athlete_data, "athlete-info")
 
 
 def get_athlete_info(athl_id):
     athlete_url = strava_url + "/pros/" + athl_id
     driver.get(athlete_url)
     ath_name = driver.find_element(By.CSS_SELECTOR, "h1.text-title1.athlete-name").text
-    athlete_location = driver.find_element(By.CSS_SELECTOR, "div.location").text
+    try:
+        athlete_location = driver.find_element(By.CSS_SELECTOR, "div.location")
+    # Perform actions with the found element
+    except NoSuchElementException:
+        athlete_location = "NA"
+
     athlete_details = {"athlete_name": ath_name,
                        "athlete_id": athl_id,
-                       "location": athlete_location}
+                       "location": athlete_location.text}
+    print(athlete_details)
     return athlete_details
 
 
 for athlete in athlete_ids:
     athlete_info = get_athlete_info(athlete.strip())
     activities_ids_list = get_activity_ids(athlete.strip())
-    print("activity ids for athlete " + athlete_info[athlete_name] + ":" + ", ".join(activities_ids_list))
+    print(activities_ids_list)
     store_activities_data(athlete_info, activities_ids_list)
     print("Data stored for athlete :" + athlete_info[athlete_name])
