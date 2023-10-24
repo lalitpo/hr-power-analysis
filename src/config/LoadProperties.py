df8eee41ae3da022aaaf2fb2bb762f039d30d867
lalitpoddar <lalit.dar4@gmail.com>
2023-10-25 01:20:47 +0530

    Solver Tester 1

--- src/config/LoadProperties.py
+++
@@ -1,7 +0,0 @@
-from jproperties import Properties
-
-configs = Properties()
-with open('../../resources/strava-config.properties', 'rb') as strava_config:
-    configs.load(strava_config)
-with open('../../resources/application-config.properties', 'rb') as app_config:
-    configs.load(app_config)
