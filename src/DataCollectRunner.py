0bc647fa03fa0e2da6ac59feac746180c6c5287e
Lalit <lalit.dar4@gmail.com>
2023-12-11 01:50:28 +0100

    Refactoring, csv and storing in DB

--- src/DataCollectRunner.py
+++ src/DataCollectRunner.py
@@ -3,26 +3,51 @@ from colorama import Fore
 from src.constants.PowerAndHRConstants import athlete_name
 from src.services.dataCollection.FetchActivityDataService import get_athlete_info, get_activity_ids, get_activities_data
 from src.services.dataCollection.LoginStravaService import login_strava
-from src.services.dataCollection.StoreActivityDataService import save_athlete_info, save_data
+from src.services.dataCollection.StoreActivityDataService import save_athlete_data
 
 athlete_ids = open('../resources/pro-athlete-id.txt').readlines()
 
+"""
 # STEP 1 : Login into strava with user account configured in strava-config properties file.
+"""
 login_strava()
 
-
+"""
 # STEP 2.a: Pull Data for each athlete.
+Retrieves athlete information and activity data based on the provided athlete ID.
+
+Args:
+    ath_id (str): The ID of the athlete.
+
+Returns:
+    tuple: A tuple containing the athlete information and activity data.
+"""
+
+
 def pull_data(ath_id):
     ath_bio = get_athlete_info(ath_id.strip())
     act_ids_list = get_activity_ids(ath_id.strip())
+    print(Fore.LIGHTWHITE_EX + "Total number of activity IDs collected : " + str(len(act_ids_list)))
     act_data = get_activities_data(act_ids_list)
     return ath_bio, act_data
 
 
+"""
 # STEP 2.b: Store Data of each athlete in the database.
+
+    Store the athlete's biography and data in the database.
+
+    Parameters:
+        bio (str): The athlete's biography.
+        data (dict): The athlete's data.
+
+    Returns:
+        None
+"""
+
+
 def store_in_db(bio, data):
-    save_athlete_info(bio, data)
-    save_data(data)
+    save_athlete_data(bio, data)
 
 
 # STEP 2 : Retrieve data from strava and store it in the database.
